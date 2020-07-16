fed22ce6f21b6f230e983755e893186d8c9b4ca3
bigspawn <bigspawn@yandex.ru>
2020-07-16 20:00:58 +0300

    docker stops

--- restart-prod.sh
+++ restart-prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 git pull
-docker-compose -f docker-compose-prod.yml up --build --force-recreate -d news notifier
+docker-compose -f docker-compose-prod.yml rm -s -f news notifier
+docker-compose -f docker-compose-prod.yml up --build -d news notifier
